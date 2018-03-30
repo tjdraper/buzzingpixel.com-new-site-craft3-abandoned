dcd6b5bc773f89bee7ea3d142a7e4df0ad023355
TJ Draper <tj@buzzingpixel.com>
2018-03-30 10:41:50 -0500

    Add navigation globals

---
+++ content/Globals/nav.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'work-with-me' => [
+        'title' => 'work with me',
+        'children' => [
+            'custom-websites' => 'Custom Websites',
+            'custom-add-ons' => 'Custom CMS Add-ons',
+            'hosting' => 'Managed CMS Hosting',
+            'portfolio' => 'Portfolio',
+        ],
+    ],
+    'news' => 'news',
+    'software' => [
+        'title' => 'software',
+        'children' => [
+            'ansel-craft' => 'Ansel for CraftCMS',
+            'ansel-ee' => 'Ansel for ExpressionEngine',
+            'treasury' => 'Treasury',
+            'ansel-treasury-ee' => 'Ansel + Treasury Bundle',
+            'construct' => 'Construct',
+            'category-construct' => 'Category Construct',
+            'collective' => 'Collective',
+            'free' => 'Free',
+        ],
+    ],
+    'support' => 'support',
+    'contact' => 'hire me',
+];
